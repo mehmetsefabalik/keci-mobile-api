9f390ea583b1ea008743e1d48b50e45341d1e206
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-03-26 09:22:37 +0300

    Add: GET /contents endpoint is added

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ mod service;
 pub struct AppState {
   listing_collection: Collection,
   product_collection: Collection,
+  content_collection: Collection,
 }
 
 #[macro_use]
@@ -19,15 +20,18 @@ async fn main() -> std::io::Result<()> {
   let db = client.database(dotenv!("DB_NAME"));
   let listing_collection = db.collection(dotenv!("DB_LISTING_COLLECTION"));
   let product_collection = db.collection(dotenv!("DB_PRODUCT_COLLECTION"));
+  let content_collection = db.collection(dotenv!("DB_CONTENT_COLLECTION"));
 
   HttpServer::new(move || {
     App::new()
       .data(AppState {
         listing_collection: listing_collection.clone(),
         product_collection: product_collection.clone(),
+        content_collection: content_collection.clone(),
       })
       .service(web::scope("/listings").route("", web::get().to(controller::listing::get)))
       .service(web::scope("/products").route("", web::get().to(controller::product::get)))
+      .service(web::scope("/contents").route("", web::get().to(controller::content::get)))
   })
   .bind("0.0.0.0:3003")?
   .run()
