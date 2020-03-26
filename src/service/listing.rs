8563978c43be385bb470679be508796beb314733
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-03-26 19:13:06 +0300

    Add: GET /listings aggregation

--- src/service/listing.rs
+++ src/service/listing.rs
@@ -1,15 +1,28 @@
 use bson::{doc, ordered};
-use mongodb::options::FindOptions;
 use mongodb::Collection;
 use std::vec;
 
 pub fn get(
   collection: Collection,
 ) -> Result<std::vec::Vec<bson::ordered::OrderedDocument>, String> {
-  match collection.find(
-    doc! {"visible": true},
-    FindOptions::builder().sort(doc! { "priority": -1 }).build(),
-  ) {
+  let pipeline = vec![
+    doc! {
+      "$match": doc! {"visible": true}
+    },
+    doc! {
+      "$lookup": doc! {"from": "product", "localField": "product_id", "foreignField": "_id", "as": "product"}
+    },
+    doc! {
+      "$unwind": doc! {"path": "$product", "preserveNullAndEmptyArrays": true}
+    },
+    doc! {
+      "$project": doc! {"product.name": 1, "product.size": 1, "product.price": 1, "product.old_price": 1, "product._id": 1}
+    },
+    doc! {
+      "$sort": doc! {"priority": -1}
+    },
+  ];
+  match collection.aggregate(pipeline.into_iter(), None) {
     Ok(cursor) => {
       let mut listings: Vec<ordered::OrderedDocument> = vec![];
       for result in cursor {
