5030879af51346069da7fc006b55482fbebf74df
Mehmet Sefa Balık <sefa.balik@trendyol.com>
2020-03-28 13:31:18 +0300

    Add: middleware which extracts token from header is added

--- src/controller/listing.rs
+++ src/controller/listing.rs
@@ -1,6 +1,7 @@
-use actix_web::{error, http, web, HttpResponse, Responder};
+use actix_web::{error, http, web, HttpResponse, Responder, HttpRequest};
 
-pub async fn get(app_data: web::Data<crate::AppState>) -> impl Responder {
+pub async fn get(req: HttpRequest, app_data: web::Data<crate::AppState>) -> impl Responder {
+  println!("user_id: {:?}", req.headers().get("user_id"));
   web::block(move || crate::service::listing::get(app_data.listing_collection.clone()))
     .await
     .map(|result| HttpResponse::Ok().json(result))
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use actix_service::Service;
+use actix_web::http::{header, HeaderValue};
 use actix_web::{web, App, HttpServer};
 use mongodb::{options::ClientOptions, Client, Collection};
 
@@ -28,6 +30,35 @@ async fn main() -> std::io::Result<()> {
         listing_collection: listing_collection.clone(),
         product_collection: product_collection.clone(),
         content_collection: content_collection.clone(),
+      })
+      .wrap_fn(|mut req, srv| {
+        let mut token = "";
+        let headers = req.headers().clone();
+        match headers.get("cookie") {
+          Some(cookie) => {
+            let split = cookie.to_str().unwrap().split(";");
+            let cookies: Vec<&str> = split.collect();
+            for cookie in &cookies {
+              if cookie.contains("access_token=") {
+                let token_cookie: Vec<&str> = cookie.split("=").collect();
+                token = token_cookie[1];
+                println!("token: {}", token);
+              }
+            }
+          }
+          None => println!("none o.o",),
+        };
+        if token.len() > 0 {
+          req.headers_mut().insert(
+            header::HeaderName::from_static("user_id"),
+            header::HeaderValue::from_str(token).unwrap(),
+          );
+        }
+        let fut = srv.call(req);
+        async {
+          let res = fut.await?;
+          Ok(res)
+        }
       })
       .service(web::scope("/listings").route("", web::get().to(controller::listing::get)))
       .service(web::scope("/contents").route("", web::get().to(controller::content::get)))
